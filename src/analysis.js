27e784161f385e3177b37f8397f50afd65bc6142
Di <didoesdigital+git@gmail.com>
2023-05-20 17:59:45 +1000

    Add analysis script

---
+++ src/analysis.js
@@ -0,0 +1,54 @@
+let sightingsData = null;
+const timeParser = d3.timeParse("%d %b %Y"); // "02 Jan 2023"
+
+function loadData() {
+  d3.csv("./data/snake-sightings-public.csv", (d) => {
+    return {
+      ...d,
+      date: timeParser(d.date),
+      temp:
+        d.temperature !== "unknown" ? d.temperature : d.estimatedTemperature,
+    };
+  }).then((data) => {
+    sightingsData = data;
+    console.log(sightingsData);
+
+    const columnsToRollup = [
+      "speciesBestGuess",
+      "seenBy",
+      "size",
+      "chunkiness",
+      "timeOfDay",
+      "temp",
+      "weather",
+      "venom",
+      "family",
+      "cyclicActivity",
+      "mating",
+      "climbing",
+      "lumps",
+      "surface",
+      "attackedByBirds",
+      "watchingMe",
+      "departure",
+      "room",
+      "yelp",
+      "onCamera",
+    ];
+    columnsToRollup.forEach((columnName) =>
+      console.log({ [columnName]: analyse(columnName) })
+    );
+  });
+}
+
+function analyse(columnName) {
+  const result = d3.rollup(
+    sightingsData,
+    (v) => v.length,
+    (d) => d[columnName]
+  );
+
+  return result;
+}
+
+loadData();

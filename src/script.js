088855beee4df11c143436a3d74ab126207cc028
Di <didoesdigital+git@gmail.com>
2023-07-08 18:51:48 +1000

    Reuse more

--- src/script.js
+++ src/script.js
@@ -1862,56 +1862,13 @@ function defensive() {
 
 function chill() {
   hideOtherChartStuff("chill");
-  chartTitle
-    .transition()
-    .duration(250)
-    .style("opacity", 0)
-    .transition()
-    .duration(250)
-    .text("Bend frends")
-    .style("opacity", 1);
 
-  simulation
-    .force(
-      "forceX",
-      d3
-        .forceX(
-          (d) =>
-            circleRadius *
-              1.25 *
-              Math.sin(
-                speciesAngleScale(d.speciesBestGuess) * (Math.PI / 180)
-              ) +
-            focalPointX
-        )
-        .strength((d) => (d["mating"] === "no mating" ? 0.8 : 1))
-    )
-    .force(
-      "forceY",
-      d3
-        .forceY(
-          (d) =>
-            circleRadius *
-              1.25 *
-              Math.cos(
-                speciesAngleScale(d.speciesBestGuess) * (Math.PI / 180)
-              ) +
-            focalPointY
-        )
-        .strength((d) => (d["mating"] === "no mating" ? 0.8 : 1))
-    )
-    // .force("charge", null)
-    .force("charge", d3.forceManyBody().strength(snekChargeStrength))
-    // .force("collide", null);
-    .force("collide", d3.forceCollide((_d) => circleRadius).strength(1));
-
-  sneks
-    .transition()
-    .duration(200)
-    .attr("fill", (d) => {
-      return speciesColorScale(d.speciesBestGuess);
-    })
-    .attr("opacity", (d) => (!didChill(d) ? 0.2 : 1));
+  updateTitle("Bend frends");
+  addSpeciesBlobForces(
+    (d) => (d["mating"] === "no mating" ? 0.8 : 1),
+    (d) => (d["mating"] === "no mating" ? 0.8 : 1)
+  );
+  addVisibleSpeciesColors((d) => (!didChill(d) ? 0.2 : 1));
 
   reheatSimulation();
 }
